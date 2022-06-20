6b1f9a493563df9802181ef1b8a125f50431b3e7
Haewon6640 <haewon6640@gmail.com>
2022-06-20 17:59:54 -0400

    fix heatlegend persisting after changing time bug

--- src/index.js
+++ src/index.js
@@ -1,82 +1,90 @@
-let mapChart = null;
 let mapRoot = null;
+var mapHeat = null;
 const loadChart = () => {
-    am5.ready(function() {
-    
+    am5.ready(function () {
         // Create root
-        var root = am5.Root.new("chartdiv"); 
-        
+        var root = am5.Root.new("chartdiv");
+
         // Set themes
         mapRoot = root;
-        root.setThemes([
-          am5themes_Animated.new(root)
-        ]);
-        
+        root.setThemes([am5themes_Animated.new(root)]);
+
         // Create chart
-        var chart = root.container.children.push(am5map.MapChart.new(root, {
-          panX: "translateX",
-          panY: "translateY",
-          layout: root.horizontalLayout
-        }));
-        
+        var chart = root.container.children.push(
+            am5map.MapChart.new(root, {
+                panX: "translateX",
+                panY: "translateY",
+                layout: root.horizontalLayout,
+            })
+        );
+
         // Create polygon series
-        // console.log(am5geodata_region_world_europeHigh)
-        var polygonSeries = chart.series.push(am5map.MapPolygonSeries.new(root, {
-          geoJSON: am5geodata_region_world_europeHigh,
-          valueField: "value",
-          calculateAggregates: true
-        }));
-        
+        var polygonSeries = chart.series.push(
+            am5map.MapPolygonSeries.new(root, {
+                geoJSON: am5geodata_region_world_europeHigh,
+                valueField: "value",
+                calculateAggregates: true,
+            })
+        );
         polygonSeries.mapPolygons.template.setAll({
-          tooltipText: "{name} \n Total Refugee:{value}"
+            tooltipText:
+                "{name} \n Total Refugee: {value} \n Percent Refugees: {percent}",
         });
-        
-        polygonSeries.set("heatRules", [{
-          target: polygonSeries.mapPolygons.template,
-          dataField: "value",
-          min: am5.color(0xff621f),
-          max: am5.color(0x661f00),
-          key: "fill"
-        }]);
-        
-        
-        polygonSeries.mapPolygons.template.events.on("pointerover", function(ev) {
-          heatLegend.showValue(ev.target.dataItem.get("value"));
-        });
-        
+
+        polygonSeries.set("heatRules", [
+            {
+                target: polygonSeries.mapPolygons.template,
+                dataField: "value",
+                min: am5.color(0xff621f),
+                max: am5.color(0x661f00),
+                key: "fill",
+            },
+        ]);
+
+        polygonSeries.mapPolygons.template.events.on(
+            "pointerover",
+            function (ev) {
+                if (!Boolean(ev.target.dataItem.get("value"))) {
+                    ev.target._settings.tooltipText = ""
+                }
+                heatLegend.showValue(ev.target.dataItem.get("value"));
+            }
+        );
+
         polygonSeries.data.setAll(data);
-        
-        var heatLegend = chart.children.push(am5.HeatLegend.new(root, {
-          orientation: "vertical",
-          startColor: am5.color(0xff621f),
-          endColor: am5.color(0x661f00),
-          startText: "Lowest",
-          endText: "Highest",
-          stepCount: 5
-        }));
-        
+
+        var heatLegend = chart.children.push(
+            am5.HeatLegend.new(root, {
+                orientation: "vertical",
+                startColor: am5.color(0xff621f),
+                endColor: am5.color(0x661f00),
+                startText: "Lowest",
+                endText: "Highest",
+                stepCount: 5,
+            })
+        );
+
         heatLegend.startLabel.setAll({
-          fontSize: 12,
-          fill: heatLegend.get("startColor")
+            fontSize: 12,
+            fill: heatLegend.get("startColor"),
         });
-        
+
         heatLegend.endLabel.setAll({
-          fontSize: 12,
-          fill: heatLegend.get("endColor")
+            fontSize: 12,
+            fill: heatLegend.get("endColor"),
         });
-        
+
         // change this to template when possible
         polygonSeries.events.on("datavalidated", function () {
-          heatLegend.set("startValue", polygonSeries.getPrivate("valueLow"));
-          heatLegend.set("endValue", polygonSeries.getPrivate("valueHigh"));
+            heatLegend.set("startValue", polygonSeries.getPrivate("valueLow"));
+            heatLegend.set("endValue", polygonSeries.getPrivate("valueHigh"));
         });
-        mapChart = polygonSeries;
-        
-        }); // end am5.ready()
-    }
+        mapHeat = heatLegend;
+    }); // end am5.ready()
+};
 
-import {refugee_per_week as rpw, refugee_percentage as rp} from "./data"; 
-console.log(rpw);
+import { refugee_per_week as rpw, refugee_percentage as rp, total_refugee_count as tc } from "./data";
+var total = document.getElementById('total-count');
 var slider = document.getElementById("myRange");
 var date = document.getElementById("date");
 var dates = [
@@ -91,83 +99,111 @@ var dates = [
     "4/26/2022 - 5/2/2022",
     "5/3/2022 - 5/9/2022",
     "5/10/2022 - 5/16/2022",
-    "5/24/2022 - 5/30/2022"]
+    "5/24/2022 - 5/30/2022",
+];
 
 date.innerHTML = dates[slider.value];
+total.innerHTML = `Total Number of Migrant Refugees: ${tc[slider.value].toLocaleString("en-US")}`;
 let country_names = Object.keys(rpw[0]);
-for (var i = 0; i < Object.keys(rpw[0]); i++) {
-
+var data = [];
+for (var i = 0; i < country_names.length; i++) {
+    // console.log(rpw[0])
+    // console.log({id: country_names[i], value: rpw[0][country_names[i]], percent: rp[0][country_names[i]]})
+    data.push({
+        id: country_names[i],
+        value: rpw[0][country_names[i]],
+        percent: `${(Math.round(rp[0][country_names[i]] * 10000) / 100).toFixed(
+            2
+        )}%`,
+    });
 }
-var data = [
-    { id: "RU", value: 234500 },
-];
 loadChart(data);
 
-slider.oninput = function() {
+// Based on Slider
+slider.oninput = function () {
     date.innerHTML = dates[this.value];
+    total.innerHTML = `Total Number of Migrant Refugees: ${tc[slider.value].toLocaleString("en-US")}`;
+    let country_names = Object.keys(rpw[this.value]);
+    var data = [];
+    for (var i = 0; i < country_names.length; i++) {
+        // console.log(rpw[0])
+        // console.log({id: country_names[i], value: rpw[0][country_names[i]], percent: rp[0][country_names[i]]})
+        data.push({
+            id: country_names[i],
+            value: rpw[this.value][country_names[i]],
+            percent: `${(Math.round(rp[this.value][country_names[i]] * 10000) / 100).toFixed(
+                2
+            )}%`,
+        });
+    }
     let root = mapRoot;
-    root.setThemes([
-        am5themes_Animated.new(root)
-      ]);
-      
-      // Create chart
-      var chart = root.container.children.push(am5map.MapChart.new(root, {
-        panX: "translateX",
-        panY: "translateY",
-        layout: root.horizontalLayout
-      }));
-      
-      // Create polygon series
-      // console.log(am5geodata_region_world_europeHigh)
-      var polygonSeries = chart.series.push(am5map.MapPolygonSeries.new(root, {
-        geoJSON: am5geodata_region_world_europeHigh,
-        valueField: "value",
-        calculateAggregates: true
-      }));
-      
-      polygonSeries.mapPolygons.template.setAll({
-        tooltipText: "{name} \n Total Refugee:{value}"
-      });
-      
-      polygonSeries.set("heatRules", [{
-        target: polygonSeries.mapPolygons.template,
-        dataField: "value",
-        min: am5.color(0xff621f),
-        max: am5.color(0x661f00),
-        key: "fill"
-      }]);
-      
-      
-      polygonSeries.mapPolygons.template.events.on("pointerover", function(ev) {
+    root.setThemes([am5themes_Animated.new(root)]);
+
+    // Create chart
+    root.container.children.pop();
+    var chart = root.container.children.push(
+        am5map.MapChart.new(root, {
+            panX: "translateX",
+            panY: "translateY",
+            layout: root.horizontalLayout,
+        })
+    );
+
+    // Create polygon series
+    // console.log(am5geodata_region_world_europeHigh)
+    var polygonSeries = chart.series.push(
+        am5map.MapPolygonSeries.new(root, {
+            geoJSON: am5geodata_region_world_europeHigh,
+            valueField: "value",
+            calculateAggregates: true,
+        })
+    );
+
+    polygonSeries.mapPolygons.template.setAll({
+        tooltipText:
+            "{name} \n Total Refugee: {value} \n Percent Refugees: {percent}",
+    });
+
+    polygonSeries.set("heatRules", [
+        {
+            target: polygonSeries.mapPolygons.template,
+            dataField: "value",
+            min: am5.color(0xff621f),
+            max: am5.color(0x661f00),
+            key: "fill",
+        },
+    ]);
+
+    polygonSeries.mapPolygons.template.events.on("pointerover", function (ev) {
+        mapHeat.hideTooltip();
+        mapHeat = heatLegend;
         heatLegend.showValue(ev.target.dataItem.get("value"));
-      });
-      var data = [
-        { id: "UA", value: 234500 },
-        ];
-      polygonSeries.data.setAll(data);
-      
-      var heatLegend = chart.children.push(am5.HeatLegend.new(root, {
-        orientation: "vertical",
-        startColor: am5.color(0xff621f),
-        endColor: am5.color(0x661f00),
-        startText: "Lowest",
-        endText: "Highest",
-        stepCount: 5
-      }));
-      
-      heatLegend.startLabel.setAll({
+    });
+    polygonSeries.data.setAll(data);
+    var heatLegend = chart.children.push(
+        am5.HeatLegend.new(root, {
+            orientation: "vertical",
+            startColor: am5.color(0xff621f),
+            endColor: am5.color(0x661f00),
+            startText: "Lowest",
+            endText: "Highest",
+            stepCount: 5,
+        })
+    );
+
+    heatLegend.startLabel.setAll({
         fontSize: 12,
-        fill: heatLegend.get("startColor")
-      });
-      
-      heatLegend.endLabel.setAll({
+        fill: heatLegend.get("startColor"),
+    });
+
+    heatLegend.endLabel.setAll({
         fontSize: 12,
-        fill: heatLegend.get("endColor")
-      });
-      
-      // change this to template when possible
-      polygonSeries.events.on("datavalidated", function () {
+        fill: heatLegend.get("endColor"),
+    });
+
+    // change this to template when possible
+    polygonSeries.events.on("datavalidated", function () {
         heatLegend.set("startValue", polygonSeries.getPrivate("valueLow"));
         heatLegend.set("endValue", polygonSeries.getPrivate("valueHigh"));
-      });
-}
+    });
+};
